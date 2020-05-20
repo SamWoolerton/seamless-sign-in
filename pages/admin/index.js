97c1c748c6a53728de6ed880636cf12064fe079a
Sam Woolerton <samuelwoolerton@gmail.com>
2020-05-20 17:01:45 +1200

    Use new mock data APIs in admin page

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,34 +1,69 @@
-export default () => (
-    <div className="h-screen bg-gray-100 pt-8">
-        <div className="text-center text-3xl">Admin UI</div>
+import fetch from "node-fetch"
+import useSWR from "swr"
 
-        <div className="flex container mx-auto mt-8">
-            <div className="card mr-6 mb-4">
-                <h3>Admin config</h3>
-                <div>
-                    <div>Set image URL</div>
-                    <input className="bg-gray-200" />
-                    <button onClick={"test"}>Reset</button>
+const asJson = url => fetch(url).then(res => res.json())
+
+export default () => {
+    const apiBaseUrl = "/.netlify/functions"
+
+    const { data: entries, error: entriesError } = useSWR(
+        `${apiBaseUrl}/entries`,
+        asJson,
+    )
+
+    const { data: active, error: activeError } = useSWR(
+        `${apiBaseUrl}/active`,
+        asJson,
+    )
+
+    return (
+        <div className="h-screen bg-gray-100 pt-8">
+            <div className="text-center text-3xl">Admin UI</div>
+
+            <div className="flex container mx-auto mt-8">
+                <div className="card mr-6 mb-4">
+                    <h3>Admin config</h3>
+                    <div>
+                        <div>Set image URL</div>
+                        <input className="bg-gray-200" />
+                        <button onClick={"test"}>Reset</button>
+                    </div>
                 </div>
-            </div>
 
-            <div className="card mr-6 mb-4">
-                <h3>List of entries</h3>
-                <div>
-                    <div>Entry 1</div>
-                    <div>Entry 2</div>
-                    <div>Entry 3</div>
+                <div className="card mr-6 mb-4">
+                    <h3>List of entries</h3>
+                    <div>
+                        {entriesError ? (
+                            <div>Hit an error loading entries.</div>
+                        ) : !entries ? (
+                            <div>Loading entries...</div>
+                        ) : (
+                            entries.map(({ id, name, direction }) => (
+                                <div key={id}>
+                                    {id} | {name} | {direction}
+                                </div>
+                            ))
+                        )}
+                    </div>
                 </div>
-            </div>
 
-            <div className="card mr-6 mb-4">
-                <h3>Haven't signed out</h3>
-                <div>
-                    <div>Entry 3</div>
-                    <div>Entry 17</div>
-                    <div>Entry 18</div>
+                <div className="card mr-6 mb-4">
+                    <h3>Haven't signed out</h3>
+                    <div>
+                        {activeError ? (
+                            <div>Hit an error loading active visitors.</div>
+                        ) : !active ? (
+                            <div>Loading visitors...</div>
+                        ) : (
+                            active.map(({ id, name }) => (
+                                <div key={id}>
+                                    {id} | {name}
+                                </div>
+                            ))
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+    )
+}
