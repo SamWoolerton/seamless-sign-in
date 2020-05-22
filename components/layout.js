74fcacbd6b5facb405cebfdf123c750becd19d23
Sam Woolerton <samuelwoolerton@gmail.com>
2020-05-22 21:09:53 +1200

    Clean up layout and add first pass at nav bar

--- components/layout.js
+++ components/layout.js
@@ -1,32 +1,25 @@
 import Head from "next/head"
-import styles from "./layout.module.scss"
 import Link from "next/link"
 
 export const siteTitle = "Seamless visitor management"
 
 export default ({ children, home }) => (
-    <div className={styles.container}>
+    <>
         <Head>
             <link rel="icon" href="/favicon.ico" />
         </Head>
-        <header className={styles.header}>
-            {home ? (
-                <>
-                    <h1>Home page</h1>
-                </>
-            ) : (
-                <>
-                    <h1>Not home page</h1>
-                </>
-            )}
-        </header>
-        <main>{children}</main>
-        {!home && (
-            <div className={styles.backToHome}>
-                <Link href="/">
-                    <a>← Back to home</a>
-                </Link>
+
+        <nav className="py-4 container mx-auto flex justify-between">
+            <div>Seamless</div>
+            <div className="flex">
+                <div className="px-2">Home</div>
+                <div className="px-2">Metrics</div>
+                <div className="px-2">Settings</div>
+                <div className="px-2">Customise</div>
+                <div className="px-2">Preview</div>
             </div>
-        )}
-    </div>
+        </nav>
+
+        <main>{children}</main>
+    </>
 )
