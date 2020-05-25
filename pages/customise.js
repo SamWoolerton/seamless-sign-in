a09365a442a96ed8daebf5dc08134bc5a178b73c
Sam Woolerton <samuelwoolerton@gmail.com>
2020-05-25 15:22:24 +1200

    Basic welcome screen customisation and persistence to localStorage

--- pages/customise.js
+++ pages/customise.js
@@ -1,25 +1,92 @@
 import Head from "next/head"
 import Link from "next/link"
+import { useState } from "react"
 import Layout, { siteTitle } from "@c/layout"
 import PreviewWelcomeScreen from "@c/preview-welcome-screen"
+import createPersistedState from "use-persisted-state"
+const useStored = createPersistedState("admin-customise")
 
-export default () => (
-    <Layout>
-        <Head>
-            <title>Customise your welcome page | {siteTitle}</title>
-        </Head>
-        <div className="container mx-auto flex">
-            <div className="relative py-12 w-2/3">
-                <PreviewWelcomeScreen />
+export default () => {
+    const [stored, setStored] = useStored({})
+
+    const [title, setTitle] = useState(stored.title)
+    const [subtitle, setSubtitle] = useState(stored.subtitle)
+    const [background, setBackground] = useState(stored.background)
+
+    return (
+        <Layout>
+            <Head>
+                <title>Customise your welcome page | {siteTitle}</title>
+            </Head>
+            <div className="container mx-auto mt-6 flex shadow-xl">
+                <div className="relative py-12 w-2/3">
+                    <PreviewWelcomeScreen
+                        {...{ title, subtitle, background }}
+                    />
+                </div>
+                <div className="w-1/3 bg-white text-gray-800 p-4 md:pb-48">
+                    <div>
+                        <h3 className="mt-2 mb-4 text-gray-700">
+                            Customise your welcome page
+                        </h3>
+                        <div>
+                            <label
+                                htmlFor="setTitle"
+                                className="font-semibold text-gray-700 mt-2"
+                            >
+                                Title
+                            </label>
+                            <input
+                                id="setTitle"
+                                className="bg-gray-100 px-2 py-1 w-full mb-2"
+                                value={title}
+                                onInput={e => setTitle(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="setSubtitle"
+                                className="font-semibold text-gray-700 mt-2"
+                            >
+                                Subtitle
+                            </label>
+                            <input
+                                id="setSubtitle"
+                                value={subtitle}
+                                className="bg-gray-100 px-2 py-1 w-full mb-2"
+                                onInput={e => setSubtitle(e.target.value)}
+                            />
+                            <div>
+                                <label
+                                    htmlFor="setBackground"
+                                    className="font-semibold text-gray-700 mt-2"
+                                >
+                                    Background image URL
+                                </label>
+                            </div>
+                            <input
+                                id="setBackground"
+                                className="bg-gray-100 px-2 py-1 w-full mb-2"
+                                value={background}
+                                onInput={e => setBackground(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <button
+                        className="button on-dark mt-4 mb-2"
+                        onClick={() =>
+                            setStored({ title, subtitle, background })
+                        }
+                    >
+                        Save changes
+                    </button>
+                </div>
             </div>
-            <div className="w-1/3 bg-gray-700 text-white p-4">
-                Controls go here
+            <div className="container mx-auto mt-12 flex justify-center">
+                <Link href="/preview">
+                    <a className="button on-dark">Preview in full-screen →</a>
+                </Link>
             </div>
-        </div>
-        <div className="container mx-auto mt-12 flex justify-center">
-            <Link href="/preview">
-                <a className="button on-dark">Preview →</a>
-            </Link>
-        </div>
-    </Layout>
-)
+        </Layout>
+    )
+}
--- pages/preview.js
+++ pages/preview.js
@@ -2,19 +2,25 @@ import Head from "next/head"
 import Link from "next/link"
 import { siteTitle } from "@c/layout"
 import PreviewWelcomeScreen from "@c/preview-welcome-screen"
+import createPersistedState from "use-persisted-state"
+const useStored = createPersistedState("admin-customise")
 
-export default () => (
-    <div>
-        <Head>
-            <title>Previewing your welcome page | {siteTitle}</title>
-        </Head>
-        <div className="relative h-screen">
-            <PreviewWelcomeScreen />
+export default () => {
+    const [stored] = useStored()
+
+    return (
+        <div>
+            <Head>
+                <title>Previewing your welcome page | {siteTitle}</title>
+            </Head>
+            <div className="relative h-screen">
+                <PreviewWelcomeScreen {...stored} />
+            </div>
+            <Link href="/customise">
+                <a className="absolute left-0 top-0 text-white no-underline cursor-pointer px-4 py-3 bg-white bg-opacity-0 hover:bg-opacity-25 transition-all duration-100">
+                    ← Return to customise
+                </a>
+            </Link>
         </div>
-        <Link href="/customise">
-            <a className="absolute left-0 top-0 text-white no-underline cursor-pointer px-4 py-3 bg-white bg-opacity-0 hover:bg-opacity-25 transition-all duration-100">
-                ← Return to customise
-            </a>
-        </Link>
-    </div>
-)
+    )
+}
