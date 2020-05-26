0d1484ccbc9e58579ab2aa7c29fbb939655602af
Sam Woolerton <samuelwoolerton@gmail.com>
2020-05-26 17:57:20 +1200

    Clean up UI for launching Unsplash search and provide feedback on URL validity

--- pages/customise.js
+++ pages/customise.js
@@ -3,6 +3,8 @@ import Link from "next/link"
 import { useState } from "react"
 import Modal from "react-modal"
 import { X } from "@styled-icons/heroicons-outline"
+import { CircleSpinner } from "react-spinners-kit"
+import { CheckCircle, ErrorAlt } from "@styled-icons/boxicons-solid"
 import Layout, { siteTitle } from "@c/layout"
 import PreviewWelcomeScreen from "@c/preview-welcome-screen"
 import createPersistedState from "use-persisted-state"
@@ -16,8 +18,10 @@ export default function CustomisePage() {
     const [title, setTitle] = useState(stored.title)
     const [subtitle, setSubtitle] = useState(stored.subtitle)
     const [background, setBackground] = useState(stored.background)
+    const [unsplashBg, setUnsplashBg] = useState(true)
+    const [imageState, setImageState] = useState("initial")
 
-    const [showModal, setShowModal] = useState(true)
+    const [showModal, setShowModal] = useState(false)
 
     useBeforeunload(() => {
         const dataSaved =
@@ -54,7 +58,7 @@ export default function CustomisePage() {
                             </label>
                             <input
                                 id="setTitle"
-                                className="bg-gray-100 px-2 py-1 w-full mb-2"
+                                className="bg-gray-200 p-2 w-full mb-2"
                                 value={title}
                                 onChange={e => setTitle(e.target.value)}
                             />
@@ -69,7 +73,7 @@ export default function CustomisePage() {
                             <input
                                 id="setSubtitle"
                                 value={subtitle}
-                                className="bg-gray-100 px-2 py-1 w-full mb-2"
+                                className="bg-gray-200 p-2 w-full mb-2"
                                 onChange={e => setSubtitle(e.target.value)}
                             />
                         </div>
@@ -78,50 +82,103 @@ export default function CustomisePage() {
                                 htmlFor="setBackground"
                                 className="font-semibold text-gray-700 mt-2"
                             >
-                                Background image URL
+                                Background image
                             </label>
-                            <div className="flex">
-                                <input
-                                    id="setBackground"
-                                    className="bg-gray-100 px-2 py-1 w-2/3 mb-2"
-                                    value={background}
-                                    onChange={e =>
-                                        setBackground(e.target.value)
-                                    }
-                                />
-                                <button
-                                    className="w-1/3"
-                                    onClick={() => setShowModal(true)}
-                                >
-                                    Open search
-                                </button>
-                                <Modal
-                                    className="container mx-auto"
-                                    overlayClassName="absolute inset-0 z-20 flex justify-center items-center bg-black bg-opacity-25"
-                                    isOpen={showModal}
-                                    onRequestClose={() => setShowModal(false)}
-                                    contentLabel="Background image picker"
-                                >
-                                    <div className="card relative flex flex-col max-h-85vh">
-                                        <X
-                                            className="absolute top-0 right-0 h-8 w-8 pt-3 pr-3 cursor-pointer text-gray-600"
-                                            onClick={() => setShowModal(false)}
-                                        >
-                                            Close
-                                        </X>
-                                        <Unsplash
-                                            onSelect={e => {
-                                                setBackground(e)
-                                                setShowModal(false)
+
+                            {/* Use hidden image load events to check whether the URL resolves to a valid image or not */}
+                            <img
+                                className="hidden"
+                                src={background}
+                                onLoad={() => setImageState("complete")}
+                                onError={() =>
+                                    background !== "" && setImageState("error")
+                                }
+                            />
+
+                            <div>
+                                {unsplashBg ? (
+                                    <button
+                                        className="w-full my-1"
+                                        onClick={() => setShowModal(true)}
+                                    >
+                                        Open Unsplash image search
+                                    </button>
+                                ) : (
+                                    <div className="relative mb-2 flex">
+                                        <input
+                                            id="setBackground"
+                                            className={
+                                                "w-full p-2 " +
+                                                (imageState === "error"
+                                                    ? "bg-red-200"
+                                                    : "bg-gray-200")
+                                            }
+                                            value={background}
+                                            onChange={e => {
+                                                setImageState(
+                                                    e.target.value === ""
+                                                        ? "initial"
+                                                        : "loading",
+                                                )
+                                                setBackground(e.target.value)
                                             }}
                                         />
+                                        <div className="bg-gray-200 p-2">
+                                            {imageState === "loading" ? (
+                                                <div className="h-8 w-8 flex justify-center items-center">
+                                                    <CircleSpinner
+                                                        color="#555"
+                                                        size={16}
+                                                    />
+                                                </div>
+                                            ) : imageState === "error" ? (
+                                                <ErrorAlt className="h-8 w-8 text-red-800" />
+                                            ) : imageState === "complete" ? (
+                                                <CheckCircle className="h-8 w-8 text-green-800" />
+                                            ) : (
+                                                <div className="h-8 w-8 text-center">
+                                                    ...
+                                                </div>
+                                            )}
+                                        </div>
                                     </div>
-                                </Modal>
+                                )}
+
+                                <div
+                                    className="underline text-gray-700 cursor-pointer mt-2"
+                                    onClick={() => setUnsplashBg(!unsplashBg)}
+                                >
+                                    {unsplashBg
+                                        ? "Enter a URL directly"
+                                        : "Use Unsplash image search"}
+                                </div>
                             </div>
+                            <Modal
+                                className="container mx-auto"
+                                overlayClassName="absolute inset-0 z-20 flex justify-center items-center bg-black bg-opacity-25"
+                                isOpen={showModal}
+                                onRequestClose={() => setShowModal(false)}
+                                contentLabel="Background image picker"
+                            >
+                                <div className="card relative flex flex-col max-h-85vh">
+                                    <X
+                                        className="absolute top-0 right-0 h-8 w-8 pt-3 pr-3 cursor-pointer text-gray-600"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Close
+                                    </X>
+                                    <Unsplash
+                                        onSelect={e => {
+                                            setBackground(e)
+                                            setShowModal(false)
+                                        }}
+                                    />
+                                </div>
+                            </Modal>
                         </div>
                     </div>
                     <button
-                        className="button on-dark mt-4 mb-2"
+                        className="button on-dark mt-8 mb-2"
                         onClick={() =>
                             setStored({ title, subtitle, background })
                         }
