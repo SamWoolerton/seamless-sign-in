011369ee1dc4c04e90e57a53dfb13c130c81edf0
Sam Woolerton <samuelwoolerton@gmail.com>
2020-05-26 18:50:57 +1200

    Improve 404 page UI

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,29 @@
+import Head from "next/head"
+import Link from "next/link"
 import Navbar from "@/components/navbar"
+import { ErrorAlt } from "@styled-icons/boxicons-solid"
+import { siteTitle } from "@/components/layout"
 
 export default function page404() {
     return (
         <>
+            <Head>
+                <title>Page not found | {siteTitle}</title>
+            </Head>
             <Navbar />
             <main>
                 <div className="container mx-auto mt-12">
                     <div className="py-6 flex flex-col justify-center text-center">
-                        <div className="flex justify-center py-2">
-                            <div className="bg-red-200 text-red-800 rounded-full font-bold py-1 px-6 text-sm">
-                                404
-                            </div>
-                        </div>
-                        <div className="text-gray-700">
-                            Sorry, that page doesn't exist.
-                        </div>
+                        <ErrorAlt className="h-12 w-12 text-red-800 mx-auto my-4" />
+                        <h3 className="text-red-800">404 | Page not found</h3>
+                        <p className="text-gray-700">
+                            Sorry, that page doesn't exist
+                        </p>
+                        <Link href="/">
+                            <a className="button on-dark inline-block mx-auto mt-8">
+                                ← Return to the home page
+                            </a>
+                        </Link>
                     </div>
                 </div>
             </main>
